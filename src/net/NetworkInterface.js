92a5289e311be5659a40ad707373d40b99f2b9cc
Christian Klein <kleinchr@ludwig-geissler-schule.de>
2026-06-09 00:03:50 +0200

    refactored HomeRouter

--- src/net/NetworkInterface.js
+++ src/net/NetworkInterface.js
@@ -99,20 +99,25 @@ export class NetworkInterface extends Observable {
      * @param {IPAddress} [opts.ip]
      * @param {number} [opts.prefixLength]
      * @param {String} [opts.name]
+     * @param {EthernetPort} [opts.port]
+     * @param {Uint8Array|number[]} [opts.mac]
      */
     constructor(opts = {}) {
         super();
 
-        // Generate a random MAC-Address for this interface
-        this.mac = new Uint8Array(6);
-        this.mac[0] = 0xAA; // private use MAC; does not collide with "real" ones
-        for (let i = 1; i < 6; i++) {
-            this.mac[i] = Math.floor(Math.random() * 256);
+        if (opts.mac) {
+            this.mac = opts.mac instanceof Uint8Array ? new Uint8Array(opts.mac) : new Uint8Array(opts.mac);
+        } else {
+            this.mac = new Uint8Array(6);
+            this.mac[0] = 0xAA; // private use MAC; does not collide with "real" ones
+            for (let i = 1; i < 6; i++) {
+                this.mac[i] = Math.floor(Math.random() * 256);
+            }
         }
 
         // @ts-ignore // KleC: aktuell wird toHex() nicht als gültige Funktion erkannt. Im Firefox geht es.
         this.name = (opts.name ?? 'enx' + this.mac.toHex());
-        this.port = new EthernetPort(this.name);
+        this.port = opts.port ?? new EthernetPort(this.name);
         this.port.subscribe(this);
 
         this._assignLinkLocal();
